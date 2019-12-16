103112e69762b2a71f2c36ba49c49a796798127f
Alasdair Sykes <alasdair.sykes@gmail.com>
2019-12-16 10:35:58 +0000

    Minor advances to 3rd pasture preprocess script.

--- Pasture preprocessing 3.R
+++ Pasture preprocessing 3.R
@@ -6,7 +6,10 @@ bigdata_repo <- "GIS data repository"
 
 Pasture_area <- find_onedrive(dir = bigdata_repo, path = "Created rasters/UK-pasture-area-10km-CLC-based-WGS84.tif") %>% raster()
 
-# read in FAO slope and elevation
-Elev <- find_onedrive(dir = bigdata_repo, path = "FAO elevation/GloElev5min.tif")
+# read in slope at 30-arc-secs from GMTED2010
+Elev <- find_onedrive(dir = bigdata_repo, path = "Created rasters/UK-dem-GMTED2010-30-arc-secs-wgs84-mean-elev-m.tif")
 
-Slope_stack <- 
+# FAO slope data at 5-arc-mins
+
+
+Slope_stack <- find_onedrive(dir = bigdata_repo, path = "")
